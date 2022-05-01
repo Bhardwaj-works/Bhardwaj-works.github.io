214c6b2be2c99db4f144831dcde1345fb1d26e93
Prince Bhardwaj <81508677+p21nc3@users.noreply.github.com>
2022-05-01 17:59:15 +0530

    Delete student directory

--- student/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import StudentResume,SkillList,ProjectList
-
-# Register your models here.
-admin.site.register(StudentResume)
-admin.site.register(SkillList)
-admin.site.register(ProjectList)
