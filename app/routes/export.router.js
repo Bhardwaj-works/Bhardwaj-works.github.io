73423b610c07b5194b3071b02a2afe44a5f86694
Prince D Yrus <pp4768@srmist.edu.in>
2022-05-22 23:35:59 +0530

    commit

--- app/routes/export.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const exportController = require('../controllers/export.controller');
-const jwtMiddleware = require('../middlewares/jwt.middleware');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.get('/resumes/:company_id', jwtMiddleware.verify, authMiddleware.ensureOfficialPlacementTeam, exportController.resumes);
-
-module.exports = router;
--- app/routes/upload.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const uploadController = require('../controllers/upload.controller');
-const jwtMiddleware = require('../middlewares/jwt.middleware');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.post('/resume', jwtMiddleware.verify, authMiddleware.ensureLoggedIn, uploadController.resume);
-
-module.exports = router;
